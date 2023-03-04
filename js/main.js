88762b1ec24f0c022fd4078be3de26d420ef33b8
sarahsameh19 <sarahsameh31@gmail.com>
2023-03-04 16:04:19 +0200

    added image slider

--- js/main.js
+++ js/main.js
@@ -27,3 +27,43 @@ humberger.addEventListener("click", () => {
     mainMenu.style.display = "none";
   }
 });
+//start of image slider 
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll(".slide-icon");
+const numberOfSlides = slides.length;
+var slideNumber = 0;
+//image slider autoplay
+var playSlider;
+
+var repeater = () => {
+  playSlider = setInterval(function () {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    slideIcons.forEach((slideIcon) => {
+      slideIcon.classList.remove("active");
+    });
+
+    slideNumber++;
+
+    if (slideNumber > (numberOfSlides - 1)) {
+      slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+  }, 4000);
+}
+repeater();
+
+//stop the image slider autoplay on mouseover
+slider.addEventListener("mouseover", () => {
+  clearInterval(playSlider);
+});
+
+//start the image slider autoplay again on mouseout
+slider.addEventListener("mouseout", () => {
+  repeater();
+});
+//end of image slider
